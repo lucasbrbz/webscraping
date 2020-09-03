d213a7600edf364fa35b3841fff196bd98fc5440
Lucas Barboza <brbzmg22@gmail.com>
2020-09-03 18:33:19 -0300

    Frontend implementado

---
+++ interface.py
@@ -0,0 +1,63 @@
+from tkinter import *
+
+class Interface():
+    window = Tk()
+    window.wm_title("Cadastro de Clientes")
+    txtNome = StringVar()
+    txtSobrenome = StringVar()
+    txtEmail = StringVar()
+    txtCPF = StringVar()
+    lblnome = Label(window, text="Nome")
+    lblsobrenome = Label(window, text="Sobrenome")
+    lblemail = Label(window, text="Email")
+    lblcpf = Label(window, text="CPF")
+    lblnome = Label(window, text="Nome")
+    lblsobrenome = Label(window, text="Sobrenome")
+    lblemail = Label(window, text="Email")
+    lblcpf = Label(window, text="CPF")
+    entNome = Entry(window, textvariable=txtNome)
+    entSobrenome = Entry(window, textvariable=txtSobrenome)
+    entEmail = Entry(window, textvariable=txtEmail)
+    entCPF = Entry(window, textvariable=txtCPF)
+    listClientes = Listbox(window)
+    scrollClientes = Scrollbar(window)
+    btnViewAll = Button(window, text="Ver todos")
+    btnBuscar = Button(window, text="Buscar")
+    btnInserir = Button(window, text="Inserir")
+    btnUpdate = Button(window, text="Atualizar Selecionados")
+    btnDel = Button(window, text="Deletar Selecionados")
+    btnClose = Button(window, text="Fechar")
+    lblnome.grid(row=0, column=0)
+    lblsobrenome.grid(row=1, column=0)
+    lblemail.grid(row=2, column=0)
+    lblcpf.grid(row=3, column=0)
+    entNome.grid(row=0, column=1, padx=50, pady=50)
+    entSobrenome.grid(row=1, column=1)
+    entEmail.grid(row=2, column=1)
+    entCPF.grid(row=3, column=1)
+    listClientes.grid(row=0, column=2, rowspan=10)
+    scrollClientes.grid(row=0, column=6, rowspan=10)
+    btnViewAll.grid(row=4, column=0, columnspan=2)
+    btnBuscar.grid(row=5, column=0, columnspan=2)
+    btnInserir.grid(row=6, column=0, columnspan=2)
+    btnUpdate.grid(row=7, column=0, columnspan=2)
+    btnDel.grid(row=8, column=0, columnspan=2)
+    btnClose.grid(row=9, column=0, columnspan=2)
+    listClientes.configure(yscrollcommand=scrollClientes.set)
+    scrollClientes.configure(command=listClientes.yview)
+    x_pad = 5
+    y_pad = 3
+    width_entry = 30
+    for child in window.winfo_children():
+        widget_class = child.__class__.__name__
+        if widget_class == "Button":
+            child.grid_configure(sticky='WE', padx=x_pad, pady=y_pad)
+        elif widget_class == "Listbox":
+            child.grid_configure(padx=0, pady=0, sticky='NS')
+        elif widget_class == "Scrollbar":
+            child.grid_configure(padx=0, pady=0, sticky='NS')
+        else:
+            child.grid_configure(padx=x_pad, pady=y_pad, sticky='N')
+
+    def run(self):
+        Interface.window.mainloop()
---
+++ main.py
@@ -0,0 +1,4 @@
+from interface import *
+app = None
+app = Interface()
+app.run()
