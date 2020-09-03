2c3dc821c04490c362f30f268174b48384f1eca1
Lucas Barboza <brbzmg22@gmail.com>
2020-09-03 20:18:48 -0300

    Tabela implementada

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 from interface import *
+from table import *
+
 app = None
 app = Interface()
 app.run()
+
+def search_command():
+    table = None
+    table = Table()
+    table.run()
+
+
+app.btnBuscar.configure(command=search_command)
